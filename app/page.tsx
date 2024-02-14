d6ac5c791c17bf4528e0b686a99ad86826ed1149
Venugopalan Iyengar <iyengarvenugopalan@gmail.com>
2024-02-15 00:25:55 +0530

    Added routes auth and root

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 14 we&apos;re coming hahaha!</h1>
-      <h2 className="h2-bold">I am the mighty Venugopalan Iyengar</h2>
-      <h3 className="h3-bold">I am worthy! I am Thor!</h3>
-    </div>
-  );
-}
